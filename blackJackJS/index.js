34eec244fcfe844635857e606d353394ea90c94b
ramez4fx <ramez4fx@gmail.com>
2022-01-28 10:03:08 -0500

    black jack

---
+++ blackJackJS/index.js
@@ -0,0 +1,10 @@
+
+
+// 1. Create two variables, firstCard and secondCard.
+let firstCard;
+let secondCard;
+// Set their values to a random number between 2-11
+firstCard = Range(2, 11);
+secondCard = Range(2, 11);
+console.log(firstCard, secondCard);
+// 2. Create a variable, sum, and set it to the sum of the two cards
